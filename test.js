b9badf4c8e7f8ded14c654e9772619f2006dc5c0
pcrantfo <pcrantfo@gmail.com>
2021-04-13 12:12:27 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+const os = require('os');
+console.log(`type: ${os.type()}`);
+
+const fs = require('fs');
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+    if (err) { throw err; }
+        console.log('data: ', data);
+        });
