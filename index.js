b9a9295b9a511dfed1b2542e64e4acce255b1659
pcrantfo <pcrantfo@gmail.com>
2021-04-21 10:13:26 -0600

    Create topMovies object array

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const topMovies = [
+    {
+        title: 'Lord of the Rings: Fellowship of the Ring',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'Lord of the Rings: The Two Towers',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'Lord of the Rings: The Return of the King',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'The Hobbit: An Unexpected Journey',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'The Hobbit: The Desolation of Smaug',
+        director: 'Peter Jackson'
+    },
+    {
+        title: 'The Hobbit: Battle of the Five Armies',
+        director: 'Peter Jackson'
+    },
+    {
+        title: `Ocean's Eleven`,
+        director: 'Steven Soderbergh'
+    },
+    {
+        title: 'Thor: Ragnarok',
+        director: 'Taika Waititi'
+    },
+    {
+        title: 'The Secret Life of Walter Mitty',
+        director: 'Ben Stiller'
+    },
+    {
+        title: 'Spaceballs',
+        director: 'Mel Brooks'
+    }
+];
+
