56659dc632de8d611514974c58f011bcd49ca33a
pcrantfo <pcrantfo@gmail.com>
2021-05-20 10:20:58 -0600

    Prevent external origin access

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ const models = require('./models.js');
 const passport = require('passport');
 require('./passport');
 
+const { check, validationResult } = require('express-validator');
+
 const movies = models.movie;
 const users = models.user;
 
 mongoose.connect('mongodb://localhost:27017/IMDBLite', { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            let message = `The CORS policy for this application doesn't allow access from origin ${origin}.`;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // middleware
 app.use(bodyParser.json());
